b44d15f5bb3e2f05c6aefe782fd0f01d3ea88451
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-09-30 11:46:22 +0200

    init commit

---
+++ src/main/java/com/weeding/rsvp/WeddingRsvpApplication.java
@@ -0,0 +1,13 @@
+package com.weeding.rsvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingRsvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingRsvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weeding/rsvp/WeddingRsvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weeding.rsvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingRsvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
